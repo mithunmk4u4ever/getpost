2147485817acb824921a7e673f243f3ce8580ca2
Mithun <mkmithun619@gmail.com>
2026-01-15 11:35:16 +0530

    error correcton

--- frontend/src/components/Students.jsx
+++ frontend/src/components/Students.jsx
@@ -1,55 +1,49 @@
-import React, { useEffect, useState } from 'react'
-import axios from "axios"
-import axiosInstance from './axiosInstance'
-
 function Students() {
-    const [name,setName]=useState("")
-    const [course,setCourse]=useState("")
-    const [students,setStudents]=useState([])
+  const [name, setName] = useState("");
+  const [course, setCourse] = useState("");
+  const [students, setStudents] = useState([]);
 
-    async function handleAddData(e){
-        e.preventDefault()
-        try {
-            const res=await axiosInstance.post("/add",{
-                name,course
-            })
-            fetchData()
-            alert(res.data)
-        } catch (error) {
-            console.log(error)
-        }
+  async function handleAddData(e) {
+    e.preventDefault();
+    try {
+      const res = await axiosInstance.post("/add", { name, course });
+      fetchData();
+      alert(res.data);
+    } catch (error) {
+      console.error(error);
     }
+  }
 
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    useEffect(()=>{
-        fetchData()
-    },[])
-
-    async function fetchData(){
-        try {
-            const res=await axiosInstance.get("/data")
-            setStudents(res.data)
-        } catch (error) {
-            console.log(error)
-        }
+  async function fetchData() {
+    try {
+      const res = await axiosInstance.get("/data");
+      setStudents(Array.isArray(res.data) ? res.data : []);
+    } catch (error) {
+      console.error(error);
+      setStudents([]);
     }
+  }
+
   return (
     <div>
-        <form action="" onSubmit={handleAddData}>
-            <input type="text" value={name} onChange={e=>setName(e.target.value)}/>
-            <input type="text" value={course} onChange={e=>setCourse(e.target.value)}/>
-            <button type='submit'>Add Data</button>
-        </form>
-        {
-          students &&  students.map(std=>
-                <div>
-                    <h3>{std.name}</h3>
-                    <h4>{std.course}</h4>
-                </div>
-            )
-        }
-    </div>
-  )
-}
+      <form onSubmit={handleAddData}>
+        <input value={name} onChange={e => setName(e.target.value)} />
+        <input value={course} onChange={e => setCourse(e.target.value)} />
+        <button type="submit">Add Data</button>
+      </form>
 
+      {Array.isArray(students) &&
+        students.map(std => (
+          <div key={std._id}>
+            <h3>{std.name}</h3>
+            <h4>{std.course}</h4>
+          </div>
+        ))}
+    </div>
+  );
+}
 export default Students
